51cb2acd4c3a2c62b37234d322e3754fc7c9ae11
hellostar06 <1918600264@qq.com>
2022-06-15 14:46:27 +0800

    空架子，只是练习上传项目到github中

---
+++ students-system/public/javascripts/db.js
@@ -0,0 +1,8 @@
+// 通过 mongoose 去连接 MongoDB
+const mongoose = require('mongoose');
+// 设置要连接的 MongoDB 服务器地址(studentsManage:要连接的数据库名称)
+const dbURI = 'mongodb://localhost:27017/studentsManage';
+// 连接数据库
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('connected', () => console.log(dbURI + ' 数据库连接成功'));
+
